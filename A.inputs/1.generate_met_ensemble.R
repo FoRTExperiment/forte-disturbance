3020e7db44ecf427e57ab72dbeb1aa8c0dd516d7
kdorheim <kalyn.dorheim@pnnl.gov>
2021-03-03 09:49:17 -0500

    Version control the met ensemble

--- A.inputs/1.generate_met_ensemble.R
+++ A.inputs/1.generate_met_ensemble.R
@@ -19,14 +19,15 @@ library(hdf5r)
 # These files have been committed to this repo for purposes of reproducibility. 
 # TODO is this legal or no? 
 BASE_DIR  <- here::here()
-PECAN_DIR <- '/people/dorh012/forte-workflow/pecan'
-assertthat::assert_that(dir.exists(PECAN_DIR))
-source(file.path(PECAN_DIR,  'models', 'ed', 'R', 'read_ed2in.R'))
-source(file.path(PECAN_DIR,  'models', 'ed', 'R', 'write_ed2in.R'))
+#PECAN_DIR <- '/people/dorh012/forte-workflow/pecan'
+#assertthat::assert_that(dir.exists(PECAN_DIR))
+#source(file.path(PECAN_DIR,  'models', 'ed', 'R', 'read_ed2in.R'))
+#source(file.path(PECAN_DIR,  'models', 'ed', 'R', 'write_ed2in.R'))
 
 # Define the directories
 BASE_DIR <- here::here()
-MET_DIR <- file.path(BASE_DIR, 'A.inputs', 'NARR-ED2-main')
+MET_DIR  <- file.path(BASE_DIR, 'A.inputs', 'NARR-ED2-main')
+WRITE_TO <- here::here("ED-outputs", "met_results")
 
 # Make sure that the met data dir exists. 
 assertthat::assert_that(dir.exists(MET_DIR))
@@ -55,13 +56,36 @@ prep_dir <- function(out_dir){
 
 # TODO there should probably be some way to itterate over this 
 # -----------------------------------------------------------------------------------------------
-met2_dir <- here::here(BASE_DIR, 'A.inputs', 'NARR-ED2-met2')
-dir.create(met2_dir, showWarnings = FALSE)
 
-# Prepare the dir and then generate the future met data as some average of data over several years.
-prep_dir(met2_dir)
-generate_future_met(out_dir = met2_dir, use_yrs = 2000:2005, startYr = 2000, endYr = 2035)
+complete_years <- 1980:2008 
 
+start <- seq(from = min(complete_years), by = 5,length.out = 8)
+end   <- start + 4
+
+periods <- mapply(function(x,y){x:y}, x = start, y = end, SIMPLIFY = FALSE)
+names(periods) <- paste0('met', 1:length(periods))
+
+
+for(m in names(periods)){
+  
+  main_dir <- here::here(BASE_DIR, 'A.inputs', 'NARR-ED2-main')
+
+  dir <- here::here(BASE_DIR, 'A.inputs', paste0('NARR-ED2_', m))
+  assert_that(!dir.exists(dir))
+  dir.create(dir, showWarnings = FALSE)
+
+  modify_met_header(dir)
+  files <- copy_files_func(list.files(main_dir, pattern = 'h5', full.names = TRUE), new_dir = dir)
+  generate_early_history(dir)
+
+  generate_future_met(out_dir = dir, use_yrs = periods[[m]], startYr = 2000, endYr = 2050)
+
+  # process the meterology files 
+  out <- procss_met_func(m, DIR = here::here("A.inputs"))
+  write.csv(out$monthly, file = file.path(WRITE_TO, paste0(m, '_monthly.csv')), row.names = FALSE)
+  write.csv(out$annual, file = file.path(WRITE_TO, paste0(m, '_yr.csv')), row.names = FALSE)
+  
+}
 
 
 
--- A.inputs/helper_functions.R
+++ A.inputs/helper_functions.R
@@ -44,7 +44,10 @@ modify_met_header <- function(to){
   metdriver_file <- file.path(here::here('A.inputs', 'NARR-ED2-main'), 'ED_MET_DRIVER_HEADER')
   met_file       <- readLines(metdriver_file)
   met_file[[1]]  <- "Modified for the forte-disturbance project"
-  met_file[[3]]  <- paste0(to, '/')
+  
+  bd <- basename(to)
+  pic_dir <- '/people/dorh012/forte-disturbance/A.inputs'
+  met_file[[3]]  <- paste0(pic_dir, '/', bd, '/')
   
   out_file <- file.path(to, 'ED_MET_DRIVER_HEADER')
   writeLines(met_file, out_file)
@@ -209,7 +212,7 @@ generate_future_met <- function(out_dir, use_yrs, startYr, endYr){
   
   # Check the inputs 
   assert_that(dir.exists(out_dir))
-  assert_that(all(is.integer(use_yrs), length(use_yrs) >= 2))  
+  assert_that(all(is.integer(use_yrs)))  
   assert_that(startYr < endYr)
   
   # Figure out which files we do have 
@@ -251,15 +254,17 @@ generate_future_met <- function(out_dir, use_yrs, startYr, endYr){
       # Caculate the daily average value for each variable 
       df <- get_nc_mean(ncs)
       
+      
       # This code was based off of https://github.com/PecanProject/pecan/blob/3b0cb56bba1eb90d1f7bbe3b0da3510f681519ba/models/ed/R/met2model.ED2.R
       # and requires hdf5 dependencies (might not work on pic). 
       temp_out <- file.path(paste0(mon, '.h5')) 
       file.copy(ncs[[1]], temp_out)
+      fill_in_h5(file = temp_out, df = df, n_steps = n_steps)
       
       
       # Now copy over the netcdf files to all of the files.
       new_files <- file.path(out_dir, paste0(2010:endYr, mon, '.h5'))
-      file.copy(temp_out, new_files)
+      file.copy(temp_out, new_files, overwrite = TRUE)
       
       # Remove the temp file
       file.remove(temp_out)
@@ -341,3 +346,98 @@ fill_in_h5 <- function(file, df, n_steps){
   return(file)
   
 }
+
+
+# Extract data from the h5 files and save as csv files 
+# 
+# Args 
+#   DIR: str directory path that contains the different met data 
+#   met: str name of the specific meteorology dir to process 
+# Return: str vector of the csv files, located in a temporary directory 
+extract_met_data <- function(DIR, met_dir){
+  
+  dir <- file.path(DIR, paste0('NARR-ED2_', met_dir))
+  
+  header_file <- list.files(dir, 'HEADER', full.names = TRUE)
+  header_contents <- readLines(header_file)
+  vars <- unlist(strsplit(header_contents[[6]], split = ' '))
+  vars <- vars[vars != 'co2']
+  
+  h5_files <- list.files(dir, '.h5', full.names = TRUE)
+  
+  results <- c()
+  temp_dir <- tempdir()
+  
+  for(path in h5_files){
+    
+    nc <- nc_open(path)
+    values <- sapply(X = vars, FUN = ncvar_get, nc = nc, USE.NAMES = TRUE)
+    values <- as.data.frame(values)
+    
+    year_month <- gsub(basename(path), pattern = '.h5', replacement = '')
+    year <- as.integer(substr(year_month, 1, 4))
+    month <- gsub(pattern = year, replacement = '', x = year_month)
+    month_list <- toupper(month.abb)
+    month <-  which(month == month_list)
+    
+    out      <- cbind(year = year, month = month,  t_index =  1:nrow(values), values)
+    out_file <- file.path(temp_dir, paste0(basename(path),'.csv'))
+    write.csv(out, file = out_file, row.names = FALSE)
+    results <- c(results, out_file)
+    
+  }
+  
+  return(results)
+  
+}
+
+
+# Calculate the monthly values from the met data files 
+# Args 
+#   files: str vector path to the files  
+# Return: a data frame of the monthly values
+get_monthly_mean <- function(files){
+  
+  # Check the inputs 
+  assert_that(all(file.exists(files)))
+  assert_that(all(grepl('h5', files)))
+  
+  # Calculate the monthly mean for each variable.
+  monthly_mean <- rbindlist(lapply(files, function(f){
+    dat  <- read.csv(f)
+    mean_vals <- apply(dat, 2, mean)
+    mean_vals <-  as.data.table(t(as.matrix(mean_vals, nrow = 1)))
+    return(mean_vals)
+  }))
+  
+  # Reshape the monthly values 
+  monthly_mean$date <- ymd(paste(monthly_mean$year, monthly_mean$month, '01', sep = '-')) 
+  monthly_mean[ , list(date, nbdsf, nddsf, vbdsf, vddsf, prate, dlwrf, pres, ugrd, vgrd, sh, tmp)] %>%  
+    melt(id.vars = c("date"), measure.vars = c("nbdsf", "nddsf", "vbdsf", "vddsf", 
+                                               "prate", 'dlwrf', "pres", "ugrd", "vgrd",
+                                               "sh", 'tmp')) -> 
+    monthly_mean 
+  
+  # Return the monthly mean 
+  return(monthly_mean)
+  
+}
+
+
+# Proccess the h5 files into average annual and average monthly values 
+# Args 
+#   met: str the name of the meteorological dir to process
+# Return: list length 2, monthly data frame and annual data frame
+procss_met_func <- function(met, DIR = "/qfs/people/dorh012/forte-disturbance/A.inputs"){
+  
+  files <- extract_met_data(DIR, met_dir = met)
+  
+  monthly_data <- get_monthly_mean(files)
+  
+  data <- monthly_data[ , list(year = year(date), value, variable)] 
+  data <- data[ , list(value = mean(value)), by = list(year, variable)]
+  
+  return(list(monthly = monthly_data, annual = data))
+  
+}
+
